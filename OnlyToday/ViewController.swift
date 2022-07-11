f6df591d9fd75d0a22b9f25111ada67a3655cf72
July <min95911@nate.com>
2022-07-11 09:10:53 +0900

    Initial Commit

---
+++ OnlyToday/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnlyToday
+//
+//  Created by 조영민 on 2022/07/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
